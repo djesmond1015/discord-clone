8c0ff1a60f65fd19d33aba74bf0bcdfd48cd4e1f
Desmond Lee <lee.desmond2016@gmail.com>
2023-09-05 22:08:29 +0800

    Add navigation-sidebar UI

---
+++ components/navigation/navigation-action.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { Plus } from 'lucide-react';
+
+import { ActionTooltip } from '@/components/action-tooltip';
+
+export const NavigationAction = () => {
+  // TODO: Add modal hook
+
+  return (
+    <div>
+      <ActionTooltip
+        side='right'
+        align='center'
+        label='Add a server'
+      >
+        <button
+          onClick={() => {}}
+          className='flex items-center group'
+          type='button'
+          aria-label='Add a server'
+        >
+          <div className='flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all items-center justify-center bg-background dark:bg-neutral-700 group-hover:bg-emerald-500'>
+            <Plus className='transition text-emerald-500 group-hover:text-white' />
+          </div>
+        </button>
+      </ActionTooltip>
+    </div>
+  );
+};

34bbcccba51d87d9377c4356c72193780b6c7ad9
Desmond Lee <lee.desmond2016@gmail.com>
2023-09-06 21:25:49 +0800

    Create ServerHeader component

---
+++ components/server/server-header.tsx
@@ -0,0 +1,87 @@
+'use client';
+
+import {
+  ChevronDown,
+  LogOut,
+  PlusCircle,
+  Settings,
+  Trash,
+  UserPlus,
+  Users,
+} from 'lucide-react';
+
+import { useModal } from '@/hooks/use-modal-store';
+import { ServerWithMembersWithProfiles } from '@/types';
+import { MemberRole } from '@prisma/client';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+
+interface ServerHeaderProps {
+  server: ServerWithMembersWithProfiles;
+  role?: MemberRole;
+}
+
+export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
+  const { onOpen } = useModal();
+
+  const isAdmin = role === MemberRole.ADMIN;
+  const isModerator = isAdmin || role === MemberRole.MODERATOR;
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger
+        className='focus:online-none'
+        asChild
+      >
+        <button className='flex items-center w-full h-12 px-3 font-semibold transition border-b-2 text-md bg-neutral-200 dark:bg-neutral-800 hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50'>
+          {server.name}
+          <ChevronDown className='w-5 h-5 ml-auto' />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className='w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]'>
+        {isModerator && (
+          <DropdownMenuItem className='px-3 py-2 text-sm text-indigo-600 cursor-pointer dark:text-indigo-400'>
+            Invite People
+            <UserPlus className='w-4 h-4 ml-auto' />
+          </DropdownMenuItem>
+        )}
+        {isAdmin && (
+          <DropdownMenuItem className='px-3 py-2 text-sm cursor-pointer '>
+            Server Settings
+            <Settings className='w-4 h-4 ml-auto' />
+          </DropdownMenuItem>
+        )}
+        {isAdmin && (
+          <DropdownMenuItem className='px-3 py-2 text-sm cursor-pointer '>
+            Manage Members
+            <Users className='w-4 h-4 ml-auto' />
+          </DropdownMenuItem>
+        )}
+        {isModerator && (
+          <DropdownMenuItem className='px-3 py-2 text-sm cursor-pointer '>
+            Create Channel
+            <PlusCircle className='w-4 h-4 ml-auto' />
+          </DropdownMenuItem>
+        )}
+        {isModerator && <DropdownMenuSeparator />}
+        {isAdmin && (
+          <DropdownMenuItem className='px-3 py-2 text-sm cursor-pointer text-rose-500 '>
+            Delete Server
+            <Trash className='w-4 h-4 ml-auto' />
+          </DropdownMenuItem>
+        )}
+        {!isAdmin && (
+          <DropdownMenuItem className='px-3 py-2 text-sm cursor-pointer text-rose-500 '>
+            Leave Server
+            <LogOut className='w-4 h-4 ml-auto' />
+          </DropdownMenuItem>
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};

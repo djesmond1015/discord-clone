52e7d224ef57aed001a3661e2dec065b85708234
Desmond Lee <lee.desmond2016@gmail.com>
2023-09-07 20:48:00 +0800

    add server setting modal and functionality

---
+++ app/api/servers/[serverId]/route.ts
@@ -0,0 +1,43 @@
+// PATCH DELETE
+
+import { CurrentProfile } from '@/lib/current-profile';
+import { db } from '@/lib/db';
+import { NextResponse } from 'next/server';
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { serverId: string } }
+) {
+  try {
+    const profile = await CurrentProfile();
+    const { name, imageUrl } = await req.json();
+
+    if (!profile) {
+      return new NextResponse('Unauthorized', { status: 401 });
+    }
+
+    if (!name) {
+      return new NextResponse('Name is required', { status: 404 });
+    }
+
+    if (!imageUrl) {
+      return new NextResponse('Image is required', { status: 404 });
+    }
+
+    const server = await db.server.update({
+      where: {
+        id: params.serverId,
+        profileId: profile.id,
+      },
+      data: {
+        name,
+        imageUrl,
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log('[SERVER_ID_PATCH]', error);
+    return new NextResponse('Internal Server Error', { status: 500 });
+  }
+}
--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 
 import { CreateServerModal } from '@/components/modals/create-server-modal';
 import { InviteModal } from '@/components/modals/invite-modal';
+import { EditServerModal } from '@/components/modals/edit-server';
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -18,8 +19,9 @@ export const ModalProvider = () => {
 
   return (
     <>
-      <CreateServerModal />
       <InviteModal />
+      <CreateServerModal />
+      <EditServerModal />
     </>
   );
 };
